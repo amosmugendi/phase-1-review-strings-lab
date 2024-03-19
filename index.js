73ec7c57608eda9d42ae43964c394d92cb69c857
amosmugendi <amosmugendi07@gmail.com>
2024-03-19 17:26:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Amos Mugendi';
+ const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+
+ const excitedWelcomeMessage= welcomeMessage.toLocaleUpperCase();
+
+ const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+ console.log(shortGreeting);
+ 
